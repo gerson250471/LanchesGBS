225cb17cc005043ee01aeb36e694726541f55e8d
Gerson Bernardo <gbds25@gmail.com>
2022-03-01 10:33:13 -0300

    Criando Classe Pedido, PedidoDetalhe e Tabelas no Banco de Dados

---
+++ LanchesGBS/Models/PedidoDetalhe.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesGBS.Models
+{
+    public class PedidoDetalhe
+    {
+        public int PedidoDetalheId { get; set; }
+
+        public int Quantidade { get; set; }
+
+        [Column(TypeName="decimal(18,2)")]
+        public decimal Preco { get; set; }
+
+        public Lanche Lanche { get; set; }
+        public int LancheId { get; set; }
+        public  Pedido Pedido { get; set; }
+        public int PedidoId { get; set; }
+    }
+}
